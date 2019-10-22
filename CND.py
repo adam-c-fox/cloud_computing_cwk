b2977c9e2801e95eb037ae61ead219cef720776b
Adam Fox <af16371@my.bristol.ac.uk>
2019-10-22 18:44:41 +0100

    SHA256^2 implemented

--- CND.py
+++ CND.py
@@ -3,8 +3,7 @@ import argparse
 import time
 
 parser = argparse.ArgumentParser(description="Let's go Nonce hunting!")
-parser.add_argument('d_val', metavar='D', type=int, #nargs='+',
-                    help='Number of leftmost 0 bits')
+parser.add_argument('d_val', metavar='D', type=int, help='Number of leftmost 0 bits')
 args = parser.parse_args()
 
 block = 'COMSM0010cloud'
@@ -13,14 +12,13 @@ D = args.d_val
 begin = time.time()
 
 for nonce in range(0, 4294967296):
-    blockandnonce = block + str(nonce)
-    hashValue = hashlib.sha256(blockandnonce.encode()).hexdigest()
-    binary = bin(int(hashValue, 16))[3:]
+    blockandnonce = block.encode('utf-8') + nonce.to_bytes(4, byteorder='big')
+    hashValue = hashlib.sha256(hashlib.sha256(blockandnonce).digest()).hexdigest()
+    binary = bin(int(hashValue, 16))[2:].zfill(256)
 
     if(int(binary[0:D]) == 0):
         termination = time.time()
         elapsed_time = termination - begin
-        #print(binary + "\n")
-        print('%.10f'%elapsed_time)
+        print(nonce)
+        print('%.10f' % elapsed_time + ' secs')
         break
-
