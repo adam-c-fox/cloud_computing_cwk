7b5578b812b0f73f4273450e158d066d7e35bdbd
Adam Fox <af16371@my.bristol.ac.uk>
2019-10-31 09:55:40 +0000

    Update remote_runner, now functioning correctly

--- remote_runner.py
+++ remote_runner.py
@@ -1,18 +1,34 @@
 import boto3
 import os
+import time
 
-#Connect
+# Connect
 ec2 = boto3.resource('ec2', region_name='us-east-2')
 
 # Create instances
-instances = ec2.create_instances(ImageId='ami-00c03f7f7f2ec15c3',
-                                InstanceType='t2.micro',
-                                MinCount=1,
-                                MaxCount=1,
-                                KeyName="AWSKeyPair1")
+instances = ec2.create_instances(ImageId='ami-00e9e85eb9ab5c284',
+                                 InstanceType='t2.micro',
+                                 MinCount=1,
+                                 MaxCount=1,
+                                 KeyName="AWSKeyPair1")
 
-for instance in instances:
-    print(instance.id, instance.instance_type)
-    #print(instance.public_dns_name)
-    #print(instance.public_ip_address)
-    #os.system('scp -i ../awskey.pem CND.py ubuntu@%s:~/' % (instance.public_ip_address))
+instance = instances[0]
+print('Waiting until running...')
+instance.wait_until_running()
+print('Running...')
+
+time.sleep(5)
+instance.reload()
+
+# Copy script to remote host
+print("Copy script to remote host...")
+os.system('scp -i keypairs/AWSKeyPair1.pem -o "StrictHostKeyChecking no" '+
+          +' CND.py ec2-user@%s:~/' % (instance.public_dns_name))
+
+# Run script on remote host
+print("Run script on remote host...")
+os.system('ssh -i keypairs/AWSKeyPair1.pem ec2-user@%s "python3 CND.py %d"' % (instance.public_dns_name, 11))
+
+# Terminate instances
+ids = [instance.id]
+ec2.instances.filter(InstanceIds=ids).terminate()
