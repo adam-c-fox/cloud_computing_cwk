294a8907d172d56f8f516a8a90c1aed1bef83db5
Adam Fox <af16371@my.bristol.ac.uk>
2019-11-03 18:47:33 +0000

    Add D parameter to remote_runner

--- remote_runner.py
+++ remote_runner.py
@@ -1,6 +1,11 @@
 import boto3
 import os
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description="Let's go Nonce hunting in the cloud!")
+parser.add_argument('d_val', metavar='D', type=int, help='Number of leftmost 0 bits')
+args = parser.parse_args()
 
 # Connect
 ec2 = boto3.resource('ec2', region_name='us-east-2')
@@ -22,12 +27,11 @@ instance.reload()
 
 # Copy script to remote host
 print("Copy script to remote host...")
-os.system('scp -i keypairs/AWSKeyPair1.pem -o "StrictHostKeyChecking no" '+
-          +' CND.py ec2-user@%s:~/' % (instance.public_dns_name))
+os.system('scp -i keypairs/AWSKeyPair1.pem -o "StrictHostKeyChecking no" CND.py ec2-user@%s:~/' % (instance.public_dns_name))
 
 # Run script on remote host
 print("Run script on remote host...")
-os.system('ssh -i keypairs/AWSKeyPair1.pem ec2-user@%s "python3 CND.py %d"' % (instance.public_dns_name, 11))
+os.system('ssh -i keypairs/AWSKeyPair1.pem ec2-user@%s "python3 CND.py %d"' % (instance.public_dns_name, args.d_val))
 
 # Terminate instances
 ids = [instance.id]
